b76506c7d43e86bebd071a4ccb23181b7ac4e742
Morgane Figard <morgane.figard@edu.ece.fr>
2019-03-12 15:43:26 +0100

    Add React FontAwesome to project

--- dashboard/src/components/Stats.js
+++ dashboard/src/components/Stats.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 export default class Stats extends Component {
   render() {
     return (
       <div>
-        <p className="text-muted">Bjr</p>
+        <h1 class="display-4">
+            <strong>150 275</strong><br />
+        </h1>
+        <big class="text-muted"><FontAwesomeIcon icon={faCoffee} /> Active customers</big>
       </div>
     )
   }
