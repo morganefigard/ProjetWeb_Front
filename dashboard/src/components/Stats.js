287890df0f39111fd4d698db72b99719cc95d7bb
Morgane Figard <morgane.figard@edu.ece.fr>
2019-03-12 16:04:55 +0100

    Change layout of Stats widget

--- dashboard/src/components/Stats.js
+++ dashboard/src/components/Stats.js
@@ -1,15 +1,16 @@
+import './Stats.css';
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
 export default class Stats extends Component {
   render() {
     return (
-      <div>
+      <div className="Stats">
         <h1 class="display-4">
-            <strong>150 275</strong><br />
+            <strong>250 275</strong><br />
         </h1>
-        <big class="text-muted"><FontAwesomeIcon icon={faCoffee} /> Active customers</big>
+        <p>Active customers <FontAwesomeIcon icon={faUsers} /></p>
       </div>
     )
   }
