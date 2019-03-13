357284bb623d3a8f4e5e84af79420861fd06bde6
Morgane Figard <morgane.figard@edu.ece.fr>
2019-03-13 15:16:29 +0100

    Change layout of Bar Chart widget

--- dashboard/src/components/BarChartWidget.js
+++ dashboard/src/components/BarChartWidget.js
@@ -1,23 +1,25 @@
+import './BarChartWidget.css';
 import React, { Component } from 'react';
 import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 
 const data = [
-    {name: 'Page A', uv: 300},
-    {name: 'Page B', uv: 200},
-    {name: 'Page C', uv: 400},
-    {name: 'Page D', uv: 500},
-    {name: 'Page E', uv: 300}
+    {name: 'Artist A', likes: 300},
+    {name: 'Artist B', likes: 200},
+    {name: 'Artist C', likes: 400},
+    {name: 'Artist D', likes: 500},
+    {name: 'Artist E', likes: 300}
     ];
 
 export default class BarChartWidget extends Component {
   render() {
     return (
-      <div>
+      <div className="BarChartWidget">
+        <h3>Number of likes per artist</h3>
         <ResponsiveContainer width='99%' height={300}>
-            <BarChart data={data}>
-                <Bar type="monotone" dataKey="uv" stroke="#8884d8" />
-                <XAxis dataKey="name" />
-                <YAxis />
+            <BarChart data={data} margin={{ top: 0, right: 0, bottom: 0, left: -25 }} >
+                <Bar radius={[10, 10, 0, 0]} dataKey="likes" fill="#f75670" />
+                <XAxis dataKey="name" stroke='#5d576b' />
+                <YAxis stroke='#5d576b' />
             </BarChart>
         </ResponsiveContainer>
       </div>
