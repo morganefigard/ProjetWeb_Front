fe7b6a55ad5ac5db4c6a3eb7f76290c678fe5969
Morgane Figard <morgane.figard@edu.ece.fr>
2019-03-25 14:03:47 +0100

    Change layout of form

--- dashboard/src/containers/Admin.js
+++ dashboard/src/containers/Admin.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
 import ArtistsForm from '../components/ArtistsForm';
+import NavBarHeader from '../components/NavbarHeader';
 
 export default class Admin extends Component {
   render() {
     return (
       <div>
-        Admin page
-        <ArtistsForm></ArtistsForm>
+        <NavBarHeader></NavBarHeader>
+        <Row>
+          <Col lg="4" md="6">
+            <ArtistsForm></ArtistsForm>
+          </Col>
+        </Row>
       </div>
     )
   }
