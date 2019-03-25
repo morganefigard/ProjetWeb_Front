1a190397499da55d82b3340db088f7fed3dfa942
Morgane Figard <morgane.figard@edu.ece.fr>
2019-03-25 16:06:24 +0100

    Add alert when an artist is added

--- dashboard/src/components/ArtistsForm.js
+++ dashboard/src/components/ArtistsForm.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button, Alert } from 'reactstrap';
 import './ArtistsForm.css'
 
 export default class ArtistsForm extends Component {
     constructor() {
         super();
 
+        this.state = {
+            visible: false,
+            artistName: "Lady Gaga"
+          };
+      
+        this.onDismiss = this.onDismiss.bind(this);
         this.addArtist = this.addArtist.bind(this);
     }
 
     addArtist() {
         console.log("Button to add artist clicked");
+        this.setState({ visible: false });
+        this.setState({ visible: true });
+    }
+    
+    onDismiss() {
+        this.setState({ visible: false });
     }
 
     render() {
@@ -29,7 +41,14 @@ export default class ArtistsForm extends Component {
                 <Label for="followers">Number of followers</Label>
                 <Input type="number" name="followers" id="followers" />
             </FormGroup>
-            <Button onClick={this.addArtist}>Add an artist</Button>
+            <FormGroup>
+                <Button onClick={this.addArtist}>Add an artist</Button>
+            </FormGroup>
+            <FormGroup>
+                <Alert color="success" isOpen={this.state.visible} toggle={this.onDismiss}>
+                    {this.state.artistName} added!
+                </Alert>
+            </FormGroup>
         </Form>
       </div>
     )
