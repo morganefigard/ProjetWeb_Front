9d0b30efcd049645c81d2fb049154ae913940079
Morgane Figard <morgane.figard@edu.ece.fr>
2019-04-04 19:45:33 +0200

    Get total number of listenings from database

--- dashboard/src/components/Stats.js
+++ dashboard/src/components/Stats.js
@@ -2,13 +2,34 @@ import './Stats.css';
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeadphones } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+
+
 
 export default class Stats extends Component {
+
+  constructor() {
+    super();
+    
+    this.state = {
+      totalNumberOfListenings: 0
+    }
+  }
+
+  componentWillMount() {
+    axios.get("http://localhost:3030/track/listeningCount").then(({ data }) => {
+      console.log(data);
+      console.log(data.total);
+      console.log(data[0].total);
+      this.setState({totalNumberOfListenings : data[0].total});
+    });
+  }
+
   render() {
     return (
       <div className="Stats">
         <h1 className="display-4">
-            <strong><FontAwesomeIcon icon={faHeadphones} /> 675</strong><br />
+            <strong><FontAwesomeIcon icon={faHeadphones} /> {this.state.totalNumberOfListenings}</strong><br />
         </h1>
         <p>Total number of listenings</p>
       </div>
