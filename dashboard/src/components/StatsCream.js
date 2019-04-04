c4d972b556287b553ed8beaf676a50caddcb714f
Morgane Figard <morgane.figard@edu.ece.fr>
2019-04-04 19:59:22 +0200

    Get mean song time from database

--- dashboard/src/components/StatsCream.js
+++ dashboard/src/components/StatsCream.js
@@ -2,13 +2,32 @@ import './StatsCream.css';
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+
 
 export default class StatsCream extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      meanDuration: 0
+    }
+  }
+
+  componentWillMount() {
+    axios.get("http://localhost:3030/track/avgTime").then(({ data }) => {
+      console.log(data);
+      console.log(data.total);
+      console.log(data[0].total);
+      this.setState({meanDuration : data[0].total});
+    });
+  }
+
   render() {
     return (
       <div className="StatsCream">
         <h1 className="display-4">
-            <strong><FontAwesomeIcon icon={faClock} /> 3:47</strong><br />
+            <strong><FontAwesomeIcon icon={faClock} /> {this.state.meanDuration}s</strong><br />
         </h1>
         <p>Mean song time</p>
       </div>
