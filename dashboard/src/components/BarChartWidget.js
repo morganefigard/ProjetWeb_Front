73f3541adace6ddfe1897288d73d17b23c198b04
Morgane Figard <morgane.figard@edu.ece.fr>
2019-04-04 19:31:33 +0200

    Get most liked songs

--- dashboard/src/components/BarChartWidget.js
+++ dashboard/src/components/BarChartWidget.js
@@ -1,33 +1,45 @@
 import './BarChartWidget.css';
 import React, { Component } from 'react';
 import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, Cell } from 'recharts';
+import axios from 'axios';
 
-const data = [
+/*const data = [
     {name: 'Track A', likes: 590},
     {name: 'Track B', likes: 457},
     {name: 'Track C', likes: 335},
     {name: 'Track D', likes: 295},
     {name: 'Track E', likes: 250},
     {name: 'Track F', likes: 190}
-    ];
+    ];*/
+
+let dataFromDB = [];
 
 const colors = ['#99e1d9', '#f75670', '#99e1d9', '#f75670', '#99e1d9', '#f75670'];
 
 export default class BarChartWidget extends Component {
+  componentWillMount() {
+    axios.get("http://localhost:3030/track/mostLiked").then(({ data }) => {
+      console.log(data);
+      for (var i=0; i<data.length; i++) {
+        dataFromDB.push({"title": data[i].title, "likes": data[i].likes});
+      }
+    })
+  }
+
   render() {
     return (
       <div className="BarChartWidget">
         <h3 className="graph-title">Top 6 most liked tracks</h3>
         <ResponsiveContainer width='99%' height={300}>
-            <BarChart data={data} margin={{ top: 0, right: 0, bottom: 0, left: -25 }} >
+            <BarChart data={dataFromDB} margin={{ top: 0, right: 0, bottom: 0, left: -25 }} >
                 <Bar radius={[10, 10, 0, 0]} dataKey="likes" fill="#f75670">
                 {
-                    data.map((entry, index) => (
+                    dataFromDB.map((entry, index) => (
                     <Cell fill={colors[index]} key={index}/>
                     ))
                 }
                 </Bar>
-                <XAxis dataKey="name" stroke='#5d576b' />
+                <XAxis dataKey="title" stroke='#5d576b' />
                 <YAxis stroke='#5d576b' />
             </BarChart>
         </ResponsiveContainer>
--- dashboard/src/components/ProgressBarsWidget.js
+++ dashboard/src/components/ProgressBarsWidget.js
@@ -1,46 +1,50 @@
 import './ProgressBarsWidget.css';
 import React, { Component } from 'react';
+import axios from 'axios';
+
+let dataFromDB = [];
 
 export default class ProgressBarsWidget extends Component {
+  componentWillMount() {
+    axios.get("http://localhost:3030/artist/topFollowers").then(({ data }) => {
+      console.log(data);
+      for (var i=0; i<data.length; i++) {
+        dataFromDB.push({"name": data[i].name, "followers": data[i].followers});
+      }
+    })
+  }
+
   render() {
     return (
       <div className="ProgressBarsWidget">
-        <h4 className="graph-title">Most listened songs of Album A</h4>
+        <h4 className="graph-title">Artists with the most followers</h4>
         <div className="table-responsive">
             <table className="table table-hover">
             <tbody>
-                <tr>
-                    <td style={{width: 26.66 + '%'}}>
-                        Song A
-                    </td>
-                    <td>
-                        <div className="progress">
-                            <div className="progress-bar-custom" role="progressbar" style={{flex: 1}}>
-                                <p className="progress-bar-text">725</p>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td style={{width: 26.66 + '%'}}>
-                        Song B
-                    </td>
-                    <td>
-                        <div className="progress">
-                            <div className="progress-bar-custom" role="progressbar" style={{flex: 0.83}}>
-                                <p className="progress-bar-text">602</p>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
+                {dataFromDB.map(function(entry, index){
+                    return (
+                        <tr key={index}>
+                            <td style={{width: 16.66 + '%'}}>
+                                {entry.name}
+                            </td>
+                            <td>
+                                <div className="progress">
+                                    <div className="progress-bar-custom" role="progressbar" style={{flex: 0.7}}>
+                                        <p className="progress-bar-text">{entry.followers}</p>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    )
+                })}
                 <tr>
                     <td style={{width: 16.66 + '%'}}>
-                        Song C
+                        Artist A
                     </td>
                     <td>
                         <div className="progress">
                             <div className="progress-bar-custom" role="progressbar" style={{flex: 0.7}}>
-                                <p className="progress-bar-text">507</p>
+                                <p className="progress-bar-text">178</p>
                             </div>
                         </div>
                     </td>
