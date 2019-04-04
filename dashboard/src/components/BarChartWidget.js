b44928fd84a52de0f5cb866b48d97e48f065f494
Morgane Figard <morgane.figard@edu.ece.fr>
2019-04-04 22:33:57 +0200

    Fix Barchart and Piechart on load: put data in state

--- dashboard/src/components/BarChartWidget.js
+++ dashboard/src/components/BarChartWidget.js
@@ -12,16 +12,26 @@ import axios from 'axios';
     {name: 'Track F', likes: 190}
     ];*/
 
-let dataFromDB = [];
+
 
 const colors = ['#99e1d9', '#f75670', '#99e1d9', '#f75670', '#99e1d9', '#f75670'];
 
 export default class BarChartWidget extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dataFromDB : []
+    }
+  }
   componentWillMount() {
     axios.get("http://localhost:3030/track/mostLiked").then(({ data }) => {
       console.log(data);
       for (var i=0; i<data.length; i++) {
-        dataFromDB.push({"title": data[i].title, "likes": data[i].likes});
+        //dataFromDB.push({"title": data[i].title, "likes": data[i].likes});
+        this.setState(prevState => ({
+          dataFromDB: [...prevState.dataFromDB, { "title": data[i].title, "likes": data[i].likes }]
+        }))
       }
     })
   }
@@ -31,10 +41,10 @@ export default class BarChartWidget extends Component {
       <div className="BarChartWidget">
         <h3 className="graph-title">Top 6 most liked tracks</h3>
         <ResponsiveContainer width='99%' height={300}>
-            <BarChart data={dataFromDB} margin={{ top: 0, right: 0, bottom: 0, left: -25 }} >
+            <BarChart data={this.state.dataFromDB} margin={{ top: 0, right: 0, bottom: 0, left: -25 }} >
                 <Bar radius={[10, 10, 0, 0]} dataKey="likes" fill="#f75670">
                 {
-                    dataFromDB.map((entry, index) => (
+                    this.state.dataFromDB.map((entry, index) => (
                     <Cell fill={colors[index]} key={index}/>
                     ))
                 }
--- dashboard/src/components/PieChartWidget.js
+++ dashboard/src/components/PieChartWidget.js
@@ -11,16 +11,27 @@ import axios from 'axios';
     {name: 'Other', songs: 1230}
     ];*/
 
-let dataFromDB = [];
+//let dataFromDB = [];
 
 const colors = ['#99e1d9', '#f75670', '#99e1d9', '#f75670', '#5d576b'];
 
 export default class PieChartWidget extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dataFromDB : []
+    }
+  }
+
   componentWillMount() {
     axios.get("http://localhost:3030/album/findGenre").then(({ data }) => {
       console.log(data);
       for (var i=0; i<data.length; i++) {
-        dataFromDB.push({"name": data[i]._id, "total": data[i].total});
+        //dataFromDB.push({"name": data[i]._id, "total": data[i].total});
+        this.setState(prevState => ({
+          dataFromDB: [...prevState.dataFromDB, { "name": data[i]._id, "total": data[i].total }]
+        }))
       }
     })
   }
@@ -31,9 +42,9 @@ export default class PieChartWidget extends Component {
         <h3 className="graph-title">Songs per genre</h3>
         <ResponsiveContainer width='99%' height={300}>
             <PieChart>
-                <Pie data={dataFromDB} dataKey="total" cx="50%" cy="40%" innerRadius={30} outerRadius={50} fill="#f75670" startAngle={90} endAngle={450} label >
+                <Pie data={this.state.dataFromDB} dataKey="total" cx="50%" cy="40%" innerRadius={30} outerRadius={50} fill="#f75670" startAngle={90} endAngle={450} label >
                 {
-                    dataFromDB.map((entry, index) => (
+                    this.state.dataFromDB.map((entry, index) => (
                     <Cell fill={colors[index]} key={index}/>
                     ))
                 }
