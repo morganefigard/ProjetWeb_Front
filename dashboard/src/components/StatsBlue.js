729054c9b2083b1e83cbaffeac84499bd4352a5a
Morgane Figard <morgane.figard@edu.ece.fr>
2019-04-09 00:43:25 +0200

    Oubli: connexion du widget bleu avec la base de données

--- dashboard/src/components/StatsBlue.js
+++ dashboard/src/components/StatsBlue.js
@@ -2,13 +2,30 @@ import './StatsBlue.css';
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+
 
 export default class StatsBlue extends Component {
+  
+  constructor() {
+    super();
+    
+    this.state = {
+      totalNumberOfLikes: 0
+    }
+  }
+
+  componentWillMount() {
+    axios.get("http://localhost:3030/track/likeCount").then(({ data }) => {
+      this.setState({totalNumberOfLikes : data[0].total});
+    });
+  }
+
   render() {
     return (
       <div className="StatsBlue">
         <h1 className="display-4">
-            <strong><FontAwesomeIcon icon={faHeart} /> 253</strong><br />
+            <strong><FontAwesomeIcon icon={faHeart} /> {this.state.totalNumberOfLikes}</strong><br />
         </h1>
         <p>Total number of likes</p>
       </div>
